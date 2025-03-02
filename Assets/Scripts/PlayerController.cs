b24f1b9b8b1ef9ad991f5eb17fee184bc6755fee
priyanshmishra888 <100042139+priyanshmishra888@users.noreply.github.com>
2025-03-02 10:36:52 +0530

    Player face direction
    Given input system and facing the same direction as it should be.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    private Rigidbody2D rb;
+    private float horizontal;
+    private float forceStrength=10f;
+    private float jumpForce=10f;
+   
+
+
+    private void Start(){
+        rb=GetComponent<Rigidbody2D>();  
+          
+    }
+    private void Update(){
+        horizontal=Input.GetAxis("Horizontal")*forceStrength;
+        if(horizontal>0){
+            transform.localScale=new Vector3(1,1,1);
+        }
         
+        else if(horizontal<0){
+            transform.localScale=new Vector3(-1,1,1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void FixedUpdate(){
         
+        rb.linearVelocity=new Vector3(horizontal,rb.linearVelocity.y,0);
     }
 }
